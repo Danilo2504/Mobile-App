d45ac72c8500e5ef228c999b8d92dfdf1ac4fcca
Dani2504 <danilobautista2004@gmail.com>
2022-07-09 16:20:02 -0300

    first commit

---
+++ App/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hola Dani</Text>
+      <TouchableOpacity onPress={()=> alert("Hola")} style={{backgroundColor:"red",height:50,width:"80%"}}>
+       <Text>Tocar</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
